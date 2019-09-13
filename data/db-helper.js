b4ded426f4295a3b72cc157cfc3f1c5ca0f48146
ElijahMcKay <45011056+ElijahMcKay@users.noreply.github.com>
2019-09-13 15:50:15 -0500

    pushing

--- data/db-helper.js
+++ data/db-helper.js
@@ -14,17 +14,18 @@ function getRecipes() {
     return db('recipes'); 
 }
 
-function getShoppingList(recipe_id) {
+function getShoppingList(id) {
     return db('recipe_ingredients as r')
-        .join("ingredients", "r.ingredient_id", "ingredients.id")
-        .where(recipe_id, "r.recipe_id")
-        .select("ingredients.name", "r.quantity")
+    .join("ingredients as i", "r.ingredient_id", "ingredients.id") // ingredients is the table we're joining recipe_ingredients with.  We only want to match where the foreign key of recipe.ingredients matches the primary ID key of of ingredients table
+    .where({recipe_id: id}) //deconstructing, recipe_id is implied from line 18
+    .select("i.name", "r.quantity") // narrowing down the columns we actually want
 }
 
-function getInstructions(recipe_id) {
-    return db('instructions as i')
-        .join('recipes', "i.recipe_id", "recipes.id")
-        .where(recipe_id, "i.recipe_id")
-        .select("steps_number", "i.instructions")
-        .orderBy("steps_number")
+function getInstructions(id) {
+    // return instructions
+    return db('instructions as i') // aliasing instructions
+        .join("recipes as r", "i.recipe_id", "r.id")  //joining with instructions with recipes, 
+        .where({recipe_id: id}) // to narrow down, we grab only the i.recipe_id where it matches the id inputed by the user
+        .select("i.steps_number", "i.instructions") // selecting the rows of the information we actually want
+        .orderBy('i.steps_number'); // sorting by the steps_number
 }
