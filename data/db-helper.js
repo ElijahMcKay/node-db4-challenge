bc284eda1d0209653659cbfd5ac5d77581824e0a
ElijahMcKay <45011056+ElijahMcKay@users.noreply.github.com>
2019-09-13 00:06:40 -0500

    helpers completed

--- data/db-helper.js
+++ data/db-helper.js
@@ -1,8 +1,8 @@
 const db = require('./db-config') 
 
-- `getRecipes()`: should return a list of all recipes in the database.
-- `getShoppingList(recipe_id)`: should return a list of all ingredients and quantities for a given recipe
-- `getInstructions(recipe_id)`: should return a list of step by step instructions for preparing a recipe
+// - `getRecipes()`: should return a list of all recipes in the database.
+// - `getShoppingList(recipe_id)`: should return a list of all ingredients and quantities for a given recipe
+// - `getInstructions(recipe_id)`: should return a list of step by step instructions for preparing a recipe
 
 module.exports = {
     getRecipes, 
@@ -22,5 +22,9 @@ function getShoppingList(recipe_id) {
 }
 
 function getInstructions(recipe_id) {
-    return db('')
+    return db('instructions as i')
+        .join('recipes', "i.recipe_id", "recipes.id")
+        .where(recipe_id, "i.recipe_id")
+        .select("steps_number", "i.instructions")
+        .orderBy("steps_number")
 }
